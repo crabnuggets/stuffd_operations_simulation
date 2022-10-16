0c6a880db1d082b1d4f08bcd97f8c30501669398
Alvin <alvinpang321@gmail.com>
2022-10-16 17:11:48 +0800

    Add 'customer' attribute to Orders to track the respective customer an order belongs to

--- customers/customer.py
+++ customers/customer.py
@@ -11,12 +11,12 @@ class Customer:
         self.inter_arrival_time = random.randint(40, 60)
         self.arrival_time = None
         self.qty_ordered = None
-        self.orders: List[Order] = None
+        self.orders: List[Order] = []
 
     def get_flow_units_ordered(self, qty_ordered):
         orders = []
         for i in range(qty_ordered):
             item_to_order = random.choice([Kebab, Burrito, DailyBowl])()
-            order = Order(item_to_order)
+            order = Order(self, item_to_order)
             orders.append(order)
         return orders
--- orders/order.py
+++ orders/order.py
@@ -1,5 +1,10 @@
+from customers.customer import Customer
+from flow_units.base_flow_unit import FlowUnit
+
+
 class Order:
-    def __init__(self, item_type) -> None:
+    def __init__(self, customer: Customer, item_type: FlowUnit) -> None:
+        self.customer = customer
         self.item_type = item_type
         self.time_created = None
         self.time_completed = None
